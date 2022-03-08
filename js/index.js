b16c6b334e4104aa16164bd74fa65ae326270c6a
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-08 14:04:21 +0100

    aggiunta ciclo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('OK');
+//conta da 0 a 100 e stampa Fizz per i multipli di 3 o Buzz per i multipli di 5
+// && FizzBuzz se sono multipli di entrambi; ()
+
+
+for(let index = 0;index <= 100; index++){ 
+    if(index % 3 === 0 && index % 5 === 0){
+        console.log('FizzBuzz'); //perchè funziona solo se la metto per prima?
+    }else if(index % 5 === 0){
+        console.log('Buzz');
+    }else if(index % 3 === 0){
+        console.log('Fizz');
+    }else{
+        console.log(index);}
+    
+}
