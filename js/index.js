1294c25af78bd9eab2feff7e81be82f50a7ce08c
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-08 17:25:36 +0100

    aggiunta stile bootstrap

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //conta da 0 a 100 e stampa Fizz per i multipli di 3 o Buzz per i multipli di 5
 // && FizzBuzz se sono multipli di entrambi; ()
 
-
+/*
 for(let index = 0;index <= 100; index++){ 
     if(index % 3 === 0 && index % 5 === 0){
         console.log('FizzBuzz'); //perchè funziona solo se la metto per prima?
@@ -12,4 +12,26 @@ for(let index = 0;index <= 100; index++){
     }else{
         console.log(index);}
     
+}
+*/
+
+
+//assegnazione  const a row && buzz && fizz
+const row = document.querySelector('#row')
+const buzz = "Buzz";
+const fizz = "Fizz"
+
+for(let index = 0;index <= 100; index++){ 
+    if(index % 3 === 0 && index % 5 === 0){
+        const element = `<div class="col-2 bg-warning p-5 text-center mx-1">${fizz}${buzz}</div>`;
+        row.innerHTML += element;
+    }else if(index % 5 === 0){
+        const element = `<div class="col-2 bg-light p-5 text-center mx-1">${buzz}</div>`;
+        row.innerHTML += element;
+    }else if(index % 3 === 0){
+        const element = `<div class="col-2 bg-danger p-5 text-center mx-1">${fizz}</div>`;
+        row.innerHTML += element;
+    }else{
+        const element = `<div class="col-2 bg-success p-5 text-center mx-1">${index}</div>`;
+        row.innerHTML += element;}
 }
